a2470bcc292a7d5363e3a5aadac6881c68cd4db8
Travis Valenti <ennuuos@gmail.com>
2017-02-20 01:28:01 +1100

    Added files
    Added Troubleshooter.py

---
+++ Troubleshooter.py
@@ -0,0 +1,9 @@
+class Troubleshooter:
+    def __init__(self):
+
+        self.health = Health()
+        self.action = Action()
+        self.knowledge = Knowledge()
+        self.secret = Secret()
+        self.equipment = Equipment()
+        self.mutation = Mutation()
