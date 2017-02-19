a172e2bfd0a31ba3b6752b816307abeec98e25ac
Travis Valenti <ennuuos@gmail.com>
2017-02-20 01:28:59 +1100

    Added files
    Added Skills.py

---
+++ Skills.py
@@ -0,0 +1,20 @@
+class Action:
+    def __init__(self):
+        self.management = {}
+        self.stealth = {}
+        self.violence = {}
+
+class Knowledge:
+    def __init__(self):
+        self.hardware = {}
+        self.software = {}
+        self.wetware = {}
+
+class Secret:
+    def __init__(self):
+        self.uncommon = {}
+        self.unlikely = {}
+        self.unhealthy = {}
+
+class Equipment:
+    pass
